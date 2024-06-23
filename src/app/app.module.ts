c3385b2cf8180d199a30e640e0a0a4ac4cda107b
Alexis Abner <alexisabner@alexiss-air.lan>
2024-06-22 18:23:48 -0600

    Add WelcomePageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
-    MovieCardComponent
+    MovieCardComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
