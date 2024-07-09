d0c2f5e7074d90b2c849b1e3abd833c70624eafa
Alexis Abner <alexisabner@alexiss-air.lan>
2024-07-08 18:23:24 -0600

    Update welcome page, and registration

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core'; // src/app/user-regist
 import { MatDialogRef } from '@angular/material/dialog'; //Closes the dialog on success
 import { FetchApiDataService } from '../fetch-api-data.service'; //API calls in fetch-api-data
 import { MatSnackBar } from '@angular/material/snack-bar'; //Displays notifications back to the user
+import { Router } from '@angular/router';
 
 //Component(defining component) and Input(defining component's input/userdata) are decorators.
 @Component({
@@ -16,7 +17,9 @@ export class UserRegistrationFormComponent implements OnInit {
   constructor(
     private fetchApiData: FetchApiDataService,
     private dialogRef: MatDialogRef<UserRegistrationFormComponent>,
-    private snackBar: MatSnackBar) { }
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
@@ -27,11 +30,40 @@ export class UserRegistrationFormComponent implements OnInit {
         this.snackBar.open('Registration successful!', 'OK', {
           duration: 2000
         });
+
+        const loginData = {
+          Username: this.user.Username,
+          Password: this.user.Password,
+        };
+        console.log('Login credentials:', loginData);
         this.dialogRef.close(); // This will close the modal on success
+        this.loginUser(loginData);
       },
-      error: (error) => {
-        console.log(error);
-        this.snackBar.open(error, 'OK', {
+      error: (err) => {
+        console.log(JSON.stringify(err));
+        this.snackBar.open(err, 'OK', {
+          duration: 2000
+        });
+      },
+    });
+  }
+
+  loginUser(loginData: any): void {
+        this.fetchApiData.userLogin(loginData).subscribe({
+      next: (response) => {
+        console.log(`LoginData: ${loginData}`)
+        localStorage.setItem('username', response.user.Username);
+        localStorage.setItem('token', response.token);
+        this.snackBar.open('Login successful!', 'OK', {
+          duration: 2000
+        });
+
+        this.router.navigate(['movies'])
+        this.dialogRef.close();
+      },
+      error: (err) => {
+        console.log('Error: ' + err);
+        this.snackBar.open(err, 'OK', {
           duration: 2000
         });
       },
